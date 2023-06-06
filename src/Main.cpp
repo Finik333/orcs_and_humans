d7f3d2f9af18ae9c26c1f4c143485f33eb257532
Oleg Petrov <petrov-o@speechpro.com>
2023-06-06 16:25:35 +0300

    Updated 1.5

--- src/Main.cpp
+++ src/Main.cpp
@@ -15,7 +15,8 @@ public:
 int main() {
     setlocale(0, "rus");
     system("mode con cols=160 lines=45");
-    for (int i=0; i < 65; i++) {
+    int vr;
+    for (int i = 0; i < 65; i++) {
         cout << " ";
     }
     cout << "ОРКИ И ЛЮДИ" << endl;
@@ -32,7 +33,7 @@ int main() {
     orcs baby;
     baby.power = 10;
     baby.hp = 35;
-            cout << "Ваша сила: " << human.power << endl;
+    cout << "Ваша сила: " << human.power << endl;
     cout << "Ваше здоровье: " << human.hp << endl;
     srand(time(NULL));
     int resoult = 1 + rand() % 3;
@@ -40,13 +41,35 @@ int main() {
     switch (resoult) {
     case 1: cout << " страшный враг маг орк" << endl;
         cout << "Сила мага: " << mag.power << endl;
-        cout << "Здоровье мага: " << mag.hp;
+        cout << "Здоровье мага: " << mag.hp<< endl;
+        cout << "Вам нужно решить что делать:1-АТАКА        2-БЕЖАТЬ" << endl;
+        cout << "Ваш выбор:";
+        cin >> vr;
+        if (vr == 1) {
+            cout << "Вы атакавали магу и сняли ему 35 единиц здоровья" << endl;
+            mag.hp = mag.hp - 35;
+            cout << "Здоровье мага: " << mag.hp << endl;
+        }
+        else {
+            srand(time(NULL));
+            int p = 1 + rand() % 2;
+            if (p == 1) {
+                cout << "Вам удалось сбежать";
+            }
+            else {
+                cout << "Вам не удалось сбежать";
+            }
+        }
+        break;
     case 2: cout << " страшный враг громила орк" << endl;
         cout << "Сила громилы: " << strongman.power << endl;
-        cout << "Здоровье громилы: " << strongman.hp;
+        cout << "Здоровье громилы: " << strongman.hp<< endl;
+        break;
     case 3: cout << " страшный враг мелкий орк" << endl;
         cout << "Сила мелкого орка: " << baby.power << endl;
-        cout << "Здоровье мелкого орка: " << baby.hp;
+        cout << "Здоровье мелкого орка: " << baby.hp << endl;
+        break;
     }
+
     return 0;
 }
